d473c983bf971280ca4b6c7972701e647d6bb07c
benjamin Dinon <benjamin.dinon@icloud.com>
2026-01-11 17:46:29 +0100

    Update Asset.java
    donner la valeur a l'asset en question. soit on lui donne la valeur donné soit on lui donne 0 pour eviter le crash

--- src/main/java/org/isep/javaprojectarchusers/Asset.java
+++ src/main/java/org/isep/javaprojectarchusers/Asset.java
@@ -3,6 +3,14 @@ package org.isep.javaprojectarchusers;
 public class Asset {
     private double value;
 
+    public Asset(){
+        this.value = 0.0;
+    }
+
+    public Asset(double value){
+        this.value = value;
+    }
+
     public String getInfo(){
         return String.valueOf(value);
     }
