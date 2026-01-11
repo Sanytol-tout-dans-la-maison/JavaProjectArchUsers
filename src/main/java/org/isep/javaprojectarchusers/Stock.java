02466d8209d354fff85aa0483d4a3f26d31b625c
benjamin Dinon <benjamin.dinon@icloud.com>
2026-01-11 17:49:13 +0100

    Update Stock.java
    ajout du nom de la compagnie lors de l'achats d'assets + Unknow si rien est renseigné pour pas que ca crash

--- src/main/java/org/isep/javaprojectarchusers/Stock.java
+++ src/main/java/org/isep/javaprojectarchusers/Stock.java
@@ -3,6 +3,16 @@ package org.isep.javaprojectarchusers;
 public class Stock extends Asset{
     private String companyName;
 
+    public Stock(String companyName, double value){
+        super(value);
+        this.companyName = companyName;
+        }
+
+    public Stock(String companyName){
+        super(0.0);
+        this.companyName = "Uknow";
+    }
+
     @Override
     public String toString(){
         return this.companyName;
