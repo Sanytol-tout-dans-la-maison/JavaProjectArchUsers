5df7e352efac9e1769f0bb39468b7506bd719ce1
Sanytol-tout-dans-la-maison <sany.delest@eleve.isep.fr>
2026-01-12 11:40:05 +0100

    Events suggestion 1

---
+++ src/main/java/org/isep/javaprojectarchusers/EVENT_TYPE.java
@@ -0,0 +1,8 @@
+package org.isep.javaprojectarchusers;
+
+public enum EVENT_TYPE {
+    Crash,
+    Lawsuit,
+    Covid_19,
+
+}
---
+++ src/main/java/org/isep/javaprojectarchusers/Events.java
@@ -0,0 +1,6 @@
+package org.isep.javaprojectarchusers;
+
+public class Events {
+    private double valueImpact;
+    private EVENT_TYPE eventType;
+}
