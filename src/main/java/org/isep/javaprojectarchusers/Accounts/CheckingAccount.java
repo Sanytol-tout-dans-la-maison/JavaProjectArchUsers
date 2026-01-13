29c16b1579497c4cdd94c99c1e42f89c0b0c2de5
Laurent Sun <lapoudrerouge@gmail.com>
2026-01-13 19:12:21 +0100

    Create class structure from the UML for SavingAccount and CheckingAccount.

--- src/main/java/org/isep/javaprojectarchusers/Accounts/CheckingAccount.java
+++ src/main/java/org/isep/javaprojectarchusers/Accounts/CheckingAccount.java
@@ -1,4 +1,21 @@
 package org.isep.javaprojectarchusers.Accounts;
 
+import org.isep.javaprojectarchusers.Portfolio;
+
+/**
+ * {@code CheckingAccount} is a subclass of {@link Account} that add functionalities specifically for paying.
+ * <p>For now nothing special except for interest rates and withdrawal limit.</p>
+ */
 public class CheckingAccount extends Account {
+    /**idk what this is*/
+    final double INTEREST_RATES;
+
+    /**Limit the user can withdraw at once?*/
+    final double WITHDRAWAL_LIMIT;
+
+    public CheckingAccount(String userName, float OVERDRAW_LIMIT, double balance, Portfolio portfolio, double INTEREST_RATES, double WITHDRAWAL_LIMIT) {
+        super(userName, OVERDRAW_LIMIT, balance, portfolio);
+        this.INTEREST_RATES = INTEREST_RATES;
+        this.WITHDRAWAL_LIMIT = WITHDRAWAL_LIMIT;
+    }
 }
