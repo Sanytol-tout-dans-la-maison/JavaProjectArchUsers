606d38246e92998990f07ce399630ba689935ad2
Laurent Sun <lapoudrerouge@gmail.com>
2026-01-14 18:44:50 +0100

    Made a change where you can open portfolio as tab but also as new stage.

--- src/main/java/org/isep/javaprojectarchusers/GUI/MainPageController.java
+++ src/main/java/org/isep/javaprojectarchusers/GUI/MainPageController.java
@@ -2,9 +2,11 @@ package org.isep.javaprojectarchusers.GUI;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
@@ -18,8 +20,9 @@ public class MainPageController {
     private TabPane portfolioHolder;
 
 
+    public void portfolioAsTab() {
+        String portfolioName = "test";
 
-    public void loadPage() {
         logger.fine("Finding resource.");
         URL resourcePath;
 
@@ -28,28 +31,73 @@ public class MainPageController {
             logger.fine("Resource path: " + resourcePath);
 
         } catch (NullPointerException e) {
-            logger.severe("The fxml file hasn't been found. "+ e);
+            logger.severe("The fxml file hasn't been found. " + e);
             throw new RuntimeException(e);
 
         }
 
+        Tab portfolioContainer = new Tab(portfolioName);
 
         logger.info("Loading FXML: PortfolioView.fxml");
         try {
-            Tab splitLayout = FXMLLoader.load(resourcePath);
+            portfolioContainer.setContent(FXMLLoader.load(resourcePath));
 
-            portfolioHolder.getTabs().add(splitLayout);
+
 
         } catch (IOException e) {
             logger.severe("IOException error during FXMLLoader.load(resourcePath): " + e);
             throw new RuntimeException(e);
 
         }
+
+        portfolioHolder.getTabs().add(portfolioContainer);
+    }
+
+
+
+    public void portfolioAsWindow() {
+        String portfolioName = "test";
+
+        logger.fine("Finding resource.");
+        URL resourcePath;
+
+        try {
+            resourcePath = Objects.requireNonNull(getClass().getResource("PortfolioView.fxml"));
+            logger.fine("Resource path: " + resourcePath);
+
+        } catch (NullPointerException e) {
+            logger.severe("The fxml file hasn't been found. " + e);
+            throw new RuntimeException(e);
+
+        }
+
+        Stage portfolioStage = new Stage();
+        AnchorPane portfolioAnchor;
+
+
+        logger.info("Loading FXML: PortfolioView.fxml");
+        try {
+            portfolioAnchor = FXMLLoader.load(resourcePath);
+
+
+        } catch (IOException e) {
+            logger.severe("IOException error during FXMLLoader.load(resourcePath): " + e);
+            throw new RuntimeException(e);
+
+        }
+        Scene scene = new Scene(portfolioAnchor);
+        portfolioStage.setScene(scene);
+        portfolioStage.show();
+
+
+
+
     }
 
     @FXML
     public void initialize() {
-        loadPage();
+        portfolioAsTab();
+        portfolioAsWindow();
 
     }
 
