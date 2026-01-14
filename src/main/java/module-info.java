1e85fd77a548fb9f3175a0f046487f16a3a38b48
Laurent Sun <lapoudrerouge@gmail.com>
2026-01-14 13:40:41 +0100

    Created a GUI folder to put all GUI element in.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,9 +4,12 @@ module org.isep.javaprojectarchusers {
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
+    requires java.logging;
 
     opens org.isep.javaprojectarchusers to javafx.fxml;
     exports org.isep.javaprojectarchusers;
     exports org.isep.javaprojectarchusers.Accounts;
     opens org.isep.javaprojectarchusers.Accounts to javafx.fxml;
+    exports org.isep.javaprojectarchusers.GUI;
+    opens org.isep.javaprojectarchusers.GUI to javafx.fxml;
 }
