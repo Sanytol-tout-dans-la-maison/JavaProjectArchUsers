7ae54deb1f6e847cbf09c27c5813a98a49a1e609
Sanytol-tout-dans-la-maison <sany.delest@eleve.isep.fr>
2026-01-16 12:15:21 +0100

    Creation of Accounts JSON and encryption of all JSON + login.csv
    All data is now completely encrypted, and it will be possible to remove the key from the Encryption Java class and have it only stored (encrypted too), so only a hashed password can open it

--- src/main/java/org/isep/javaprojectarchusers/LoginExtraction.java
+++ src/main/java/org/isep/javaprojectarchusers/LoginExtraction.java
@@ -4,11 +4,12 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class LoginExtraction {
-    public static void extract(ArrayList<String> email, ArrayList<String> password) throws IOException {
-        ArrayList<String> data = FileReaderWithBuffer.readCSV("src/main/resources/org/isep/javaprojectarchusers/login.csv");
-        for(int i = 0; i < data.size(); i += 2){
+    public static void extract(ArrayList<String> email, ArrayList<String> password, ArrayList<String> key) throws IOException {
+        ArrayList<String> data = FileReaderWithBuffer.readCSV("src/main/resources/org/isep/javaprojectarchusers/GUI/login.csv");
+        for(int i = 0; i < data.size(); i += 3){
             email.add(data.get(i));
             password.add(data.get(i+1));
+            key.add(data.get(i+2));
         }
     }
 }
